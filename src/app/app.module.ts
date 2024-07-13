01605e92f745994f63c5ec8a6761a547d98fe90e
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-07-13 04:17:28 +0300

    Create No Data Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoDataComponent } from './no-data/no-data.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoDataComponent
   ],
   imports: [
     BrowserModule,
